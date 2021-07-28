795996326ac38a1d4d98ebe5b8603455d2945fb7
Abdul786Qadir <86151785+Abdul786Qadir@users.noreply.github.com>
2021-07-28 14:59:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World hi abdul qadir!");
+        System.out.println("Hello hi abdul qadir!");
     }
 }
 
